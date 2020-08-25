bcb738b59a3530ac38b39f03b266bfe554d62cf5
realhome <boqian.cheng@yahoo.com>
2020-08-25 08:44:13 -0700

    mini change

--- RobotsPencils/MyDesign/NetworkServiceFactory.swift
+++ RobotsPencils/MyDesign/NetworkServiceFactory.swift
@@ -19,20 +19,20 @@ struct NetworkServiceFactory {
     static func createNetworkService(serviceType: NetworkServiceType) -> NetWorkService {
         switch serviceType {
         case .restful:
-            return restfulService()
+            return RestfulService()
         case .graphGL:
-            return graphGLService()
+            return GraphGLService()
         }
     }
 }
 
-class restfulService: NetWorkService {
+class RestfulService: NetWorkService {
     func saveData(data: DataType, completion: (() -> Void)?) {
         // the restful api implementation
     }
 }
 
-class graphGLService: NetWorkService {
+class GraphGLService: NetWorkService {
     func saveData(data: DataType, completion: (() -> Void)?) {
         // the grapgGL api implementation
     }
